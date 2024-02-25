ad1163722eb312a2973df262ecd87ea44cb31f6c
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 18:28:44 -0500

    Changed the file name

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def convert(srcpath,dstpath):
         filepath = srcpath + "/" + file
         if os.path.isfile(filepath):
             print("Starting", filepath)
-            output = dstpath + "/" + file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
+            output = dstpath + "/" + file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + file[16:18] + ".mp4"
             if not os.path.exists(output):
                 print(output)
                 result = subprocess.run(["HandBrakeCLI", "-Z", "Very Fast 2160p60 4K AV1","-i",filepath,"-o",output])
