eddca289b9ffe61d500d3cbee5cfb2b4e6ec4929
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 18:01:30 -0500

    Changed the result

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import logging
 def convert(srcpath,dstpath):
     files = os.listdir(srcpath)
     for file in files:
-        print("Starting",file)
-        os.path.isfile(srcpath + "/" + file)
-        output = file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
-        print(output)
-        if not os.path.isfile(output):
-            result = subprocess.run(["HandBrakeCLI -Z \"Very Fast 2160p60 4K AV1\" -i " + srcpath + "/" + file + " -o " + output])
-            print(result.args)
-        print("Finished",file,output)
+        if os.path.isfile(srcpath + "/" + file):
+            print("Starting", file)
+            output = dstpath + "/" + file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
+            if not os.path.isfile(output):
+                result = subprocess.run(["HandBrakeCLI", "-Z", "Very Fast 2160p60 4K AV1","-i", srcpath + "/" + file,"-o",output])
+                print(result.args)
+            print("Finished",file,output)
 
 
 # Press the green button in the gutter to run the script.
