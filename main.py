82ca1b7e0170d3369f5daf56806f462114dcd5b3
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 17:39:13 -0500

    Removed the completed

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import logging
 def convert(srcpath,dstpath):
     files = os.listdir(srcpath)
     for file in files:
+        print("Starting",file)
         os.path.isfile(srcpath + "/" + file)
         output = file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
         if not os.path.isfile(dstpath + "/" + output):
             subprocess.run(["HandBrakeCLI", "-Z \"Very Fast 2160p60 4K AV1\"","-i" + srcpath + "/" + file,"-o" + dstpath + "/" + output])
-        print(file,output,subprocess.CompletedProcess())
+        print("Finished",file,output)
 
 
 # Press the green button in the gutter to run the script.
