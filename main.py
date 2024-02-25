6176a64203a2055d309af71a012c734bd6efd5bb
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 18:13:41 -0500

    Test the output

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import logging
 def convert(srcpath,dstpath):
     files = os.listdir(srcpath)
     for file in files:
-        file = srcpath + "/" + file
-        if os.path.isfile(file):
-            print("Starting", file)
+        filepath = srcpath + "/" + file
+        if os.path.isfile(filepath):
+            print("Starting", filepath)
             output = dstpath + "/" + file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
             if not os.path.exists(output):
                 print(output)
@@ -19,8 +19,6 @@ def convert(srcpath,dstpath):
                 os.remove(file)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     convert('/mnt/dados/DashCam','/mnt/dados/DashCam/Converted')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
