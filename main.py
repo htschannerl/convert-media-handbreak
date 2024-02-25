3fc054f56a434547790013b637fe38fdd7a1fd80
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 18:06:59 -0500

    Changed to remove the source file

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import logging
 def convert(srcpath,dstpath):
     files = os.listdir(srcpath)
     for file in files:
-        if os.path.isfile(srcpath + "/" + file):
+        file = srcpath + "/" + file
+        if os.path.isfile(file):
             print("Starting", file)
             output = dstpath + "/" + file[0:4] + "-" + file[4:6] + "-" + file[6:8] + "_" + file[8:10] + "-" + file[10:12] + "-" + file[12:14] + "-" + file[14:16] + file[16:18] + ".mp4"
             if not os.path.isfile(output):
                 result = subprocess.run(["HandBrakeCLI", "-Z", "Very Fast 2160p60 4K AV1","-i", srcpath + "/" + file,"-o",output])
                 if result.returncode == 0:
                     print("Finished",file,output)
+                    os.remove(file)
+            else:
+                print(file,"already exist removing the source")
+                os.remove(file)
 
 
 # Press the green button in the gutter to run the script.
