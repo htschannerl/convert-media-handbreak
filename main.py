4bc812a200a4b56e696a3376728b35e5552eaf28
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-02-25 22:32:55 -0500

    Removed the season

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def convert(srcpath,dstpath):
             print("Starting", filepath)
             t = datetime.datetime(int(file[0:4]), int(file[4:6]), int(file[6:8]), int(file[8:10]), int(file[10:12]), int(file[12:14]))
             t = t.strftime('%a %d %B %Y, %H:%M:%S')
-            output = dstpath + "/S01E" + str(total).zfill(8) + "." + str(t) + ".mp4"
+            output = dstpath + "/" + str(t) + ".mp4"
             if not os.path.exists(output):
                 print(output)
                 result = subprocess.run(["/usr/bin/HandBrakeCLI", "-Z", "Very Fast 2160p60 4K HEVC","-i",filepath,"-o",output])
