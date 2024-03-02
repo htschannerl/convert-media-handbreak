a02326fd42b12204c6e3d9b9af1d4170e07a1a92
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-03-02 16:01:23 -0500

    Test of rename file

--- main.py
+++ main.py
@@ -46,19 +46,20 @@ def renameFile(srcpath,dstpath):
     dstfiles = sorted(dstfiles)
     dstTotal = len(dstfiles)
     srcTotal = len(srcfiles)
-    for srcfile in srcfiles:
-        print(srcfile)
 
-    print("*********************************")
+    print(srcfiles)
 
     for dstfile in dstfiles:
         epsode = str(count).zfill(5)
         dstfile = dstfile.replace(".S01E" + epsode,"")
         dstfiles[count - 1] = dstfile
-        print(dstfile)
         count = count + 1
+
+    for srcfile in srcfiles:
+        print(srcfile)
+
+    print(srcfiles)
     print(srcTotal,"of",dstTotal)
-    print(dstfiles)
 
 if __name__ == '__main__':
     #convert('/mnt/dados/DashCam/Origin','/mnt/dados/DashCam/Converted')
