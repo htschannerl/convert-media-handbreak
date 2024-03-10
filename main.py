c6ae7fa3c67644b40f0a205c4ab40e1f0cdb68be
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-03-10 18:24:02 -0400

    Changed the log position

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ def convertNew(srcpath,dstpath):
                     logging.info("Converting " + srcfile + " => " + output)
                     result = subprocess.run(["/usr/bin/HandBrakeCLI", "-Z", "Very Fast 2160p60 4K HEVC", "-i", filepath, "-o", output],stdout=subprocess.DEVNULL,stderr=subprocess.PIPE)
                     if result.returncode == 0:
-                        print("Converted",srcfile,"=>",output)
                         logging.info("Converted " + srcfile + " => " + output)
+                        print("Converted",srcfile,"=>",output)
                     #    os.remove(filepath)
                     else:
                         logging.error("Error " + srcfile + " => " + output)
