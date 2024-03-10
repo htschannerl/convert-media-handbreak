a909fb1d640b3aca098ce98e8a4efa427d3a5897
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-03-10 00:31:06 -0500

    Changed to not convert

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ def convertNew(srcpath,dstpath):
             output = dstpath + "/" + output + "." + epsode + ".mp4"
             print("Converting",srcfile,"=>",output)
             #result = subprocess.run(["/usr/bin/HandBrakeCLI", "-Z", "Very Fast 2160p60 4K HEVC", "-i", filepath, "-o", output],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-            if result.returncode == 0:
-                dstTotal = dstTotal + 1
-                print("Converted",srcfile,"=>",output)
+           # if result.returncode == 0:
+            dstTotal = dstTotal + 1
+            print("Converted",srcfile,"=>",output)
             #    os.remove(filepath)
 
 def rename(srcpath,dstpath):
