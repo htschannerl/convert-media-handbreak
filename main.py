625339dbd572987890433de1363fb77a717d29c8
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-03-29 23:12:47 -0400

    Added the diff

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def convertNew(srcpath,dstpath,preset):
                 srcstat = os.stat(filepath)
                 if output in dstfiles:
                     dststat = os.stat(dstpath + "/" + output)
-                    data = [output, round(srcstat.st_size / (1024 * 1024),2), round(dststat.st_size / (1024 * 1024),2)]
+                    data = [output, round(srcstat.st_size / (1024 * 1024),2), round(dststat.st_size / (1024 * 1024),2),round(srcstat.st_size / (1024 * 1024),2) - round(dststat.st_size / (1024 * 1024),2)]
                     df.loc[srcfile] = data
                     print(srcfile, "already exist removing the source",str(round(srcstat.st_size / (1024 * 1024),2)),"-",str(round(dststat.st_size / (1024 * 1024),2)))
                     logging.info(srcfile + " already exist removing the source. Removing it from the source")
@@ -72,7 +72,7 @@ def convertNew(srcpath,dstpath,preset):
                     result = subprocess.run(["/usr/bin/HandBrakeCLI", "-Z", preset, "-i", filepath, "-o", output],stdout=subprocess.DEVNULL,stderr=subprocess.PIPE,env=my_env)
                     if result.returncode == 0:
                         dststat = os.stat(dstpath + "/" + output)
-                        data = [output, round(srcstat.st_size / (1024 * 1024), 2), round(dststat.st_size / (1024 * 1024), 2)]
+                        data = [output, round(srcstat.st_size / (1024 * 1024),2), round(dststat.st_size / (1024 * 1024),2),round(srcstat.st_size / (1024 * 1024),2) - round(dststat.st_size / (1024 * 1024),2)]
                         df.loc[srcfile] = data
                         logging.info("Converted " + srcfile + " => " + output)
                         print("Converted",srcfile,"=>",output,"-",str(round(srcstat.st_size / (1024 * 1024),2)),"-",str(round(dststat.st_size / (1024 * 1024),2)))
