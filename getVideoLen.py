21651d8065c987e549d727aed92f2028bb4d3871
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-05-15 18:29:06 -0400

    Test to get video lenght

--- getVideoLen.py
+++ getVideoLen.py
@@ -1,11 +1,16 @@
+# import module
 import cv2
+import datetime
 
 # create video capture object
-cap = cv2.VideoCapture('/mnt/dados/DashCam/Converted/Front/2024-05-14_07-57-48.mp4')
+data = cv2.VideoCapture('/mnt/dados/DashCam/Converted/Front/2024-05-14_07-57-48.mp4')
 
 # count the number of frames
-fps = cap.get(cv2.CAP_PROP_FPS)
-totalNoFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-durationInSeconds = totalNoFrames // fps
+frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+fps = data.get(cv2.CAP_PROP_FPS)
 
-print("Video Duration In Seconds:", durationInSeconds, "s")
+# calculate duration of the video
+seconds = round(frames / fps)
+video_time = datetime.timedelta(seconds=seconds)
+print(f"duration in seconds: {seconds}")
+print(f"video time: {video_time}")
