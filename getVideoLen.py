434538c2f19f22b0337a07370e2102aa5d1e12c0
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-05-15 20:24:53 -0400

    Fixed the data

--- getVideoLen.py
+++ getVideoLen.py
@@ -16,7 +16,6 @@ def getVideoLen(file):
     seconds = round(frames / fps)
     video_time = datetime.timedelta(seconds=seconds)
     result = [seconds, str(video_time)]
-    print(result)
     return result
 
 def updateLenFile(file):
