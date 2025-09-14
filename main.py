a4e525784197a84390986da117da57bbb5638174
Henrique Tschannerl <h.tschannerl@gmail.com>
2025-09-13 21:16:31 -0400

    Changed to log

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ class main:
                     backup = row["backup"]
                     if os.path.isfile(src):
                         print(f"Compressing the file {src}")
+                        logging.info(f"Compressing the file {src}")
                         if compress:
                             result = subprocess.run(["/usr/bin/7z", "a",f"{dst}/{cam}.7z",src,"-t7z","-m0=LZMA2","-mx=9","-md=1024m","-mfb=64","-ms=on","-mmt=on"],
                                                     stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, env=my_env)
@@ -66,6 +67,7 @@ class main:
                                 df.loc[index,"backup"] = "yes"
                                 os.remove(src)
                     else:
+                        logging.info(f"The file {src} is not in the folder and the status is {backup}")
                         print(f"The file {src} is not in the folder and the status is {backup}")
 
         df = df.set_index('srcfile')
