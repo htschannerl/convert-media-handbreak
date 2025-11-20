facff519bb4fc8464c7ae644a812e770dede6734
Henrique Tschannerl <h.tschannerl@gmail.com>
2025-11-19 23:39:10 -0500

    Changed to not use preset

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ class main:
                     output = dstpath + "/" + output
                     print("Converting",srcfile,"=>",output)
                     logging.info("Converting " + srcfile + " => " + output)
-                    result = subprocess.run(["/usr/bin/HandBrakeCLI", "-Z", preset, "-i", filepath, "-o", output],stdout=subprocess.DEVNULL,stderr=subprocess.PIPE,env=my_env)
+                    result = subprocess.run(["/usr/bin/HandBrakeCLI", "-i", filepath, "-o", output, "-e","x265","-q","28","--cfr"],stdout=subprocess.DEVNULL,stderr=subprocess.PIPE,env=my_env)
                     if result.returncode == 0:
                         dststat = os.stat(output)
                         lenVideo = getVideoLen.getVideoLen(output)
